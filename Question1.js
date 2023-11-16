033cd8cb93fd34db612bfc2fb8f736baa0518653
Ramajyothi55 <106086959+Ramajyothi55@users.noreply.github.com>
2023-11-16 19:53:35 +0530

    Update Question1.js

--- Question1.js
+++ Question1.js
@@ -1,1 +1,19 @@
+function arraypair(array,sum){
+    for (i = 0;i < array.length;i++) {
+        var first = array[i];
+        for (j = i + 1;j < array.length;j++) {
+            var second = array[j];
+
+            if ((first + second) == sum) {
+        //alert('First: ' + first + ' Second ' + second + ' SUM ' + sum);
+        console.log('First: ' + first + ' Second ' + second + ' Sum is '+sum);
+            }
+        }
+
+    }
+}
+
+var a = [2, 4, 3, 5, 6, -2, 4, 7, 8, 9];
+
+arraypair(a,7);
 
