f24583e7e333b027d0f3685f4399a5ff9eb33a99
Ramajyothi55 <106086959+Ramajyothi55@users.noreply.github.com>
2023-11-17 19:44:50 +0530

    Create Q7.PrefixtoInfix.js

---
+++ Q7.PrefixtoInfix.js
@@ -0,0 +1,37 @@
+ function convert(str)
+    {
+        let stack = [];
+ 
+        // Length of expression
+        let l = str.length;
+ 
+    
+        for(let i = l - 1; i >= 0; i--)
+        {
+            let c = str[i];
+ 
+            if (isOperator(c))
+            {
+                let op1 = stack[stack.length - 1];
+                stack.pop()
+                let op2 = stack[stack.length - 1];
+                stack.pop()
+ 
+            
+                let temp = "(" + op1 + c + op2 + ")";
+                stack.push(temp);
+            }
+            else
+            {
+ 
+            
+                stack.push(c + "");
+            }
+        }
+        return stack[stack.length - 1];
+    }
+     
+    let exp = "*+AB-CD";
+      
+    document.write("Infix : " + convert(exp));
+   
